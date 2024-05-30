82181b867cf1ccd6cad6aed71f02400a924271e3
Ansh Bhasin <74207421+ItzAnsh@users.noreply.github.com>
2024-05-30 16:30:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ app.use("/api/users", userRouter);
 app.use("/api/admin", adminRouter);
 app.use("/api/librarian", librarianRouter);
 
+app.get("/", (req, res) => {
+	res.send("API is working fine :)");
+});
+
 // Start Server
 app.listen(3000, connectDB(), async () => {
 	console.log("Server is running on port 3000");
