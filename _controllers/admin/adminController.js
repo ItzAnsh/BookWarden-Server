2c1bb54cd0aef2489861d337c20e18a6d0abe2a8
ItzAnsh <anshbhasin3723@gmail.com>
2024-06-05 11:55:51 +0530

    Fix deployment

--- _controllers/admin/adminController.js
+++ _controllers/admin/adminController.js
@@ -8,254 +8,295 @@ import { sendWelcomeEmail } from "../../lib/nodemailer.js";
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
 
-
-
 dotenv.config();
 
 const getAllUsers = AsyncErrorHandler(async (req, res) => {
-  const users = await User.find();
-  res.json(users);
+	const users = await User.find();
+	res.json(users);
 });
 
 const getSpecificUser = AsyncErrorHandler(async (req, res) => {
-  const { userId } = req.params;
-  if (!userId) {
-    res.status(400);
-  }
+	const { userId } = req.params;
+	if (!userId) {
+		res.status(400);
+	}
 
-  const user = await User.findById({ _id: mongoose.Types.ObjectId(userId) });
-  res.json(user);
+	const user = await User.findById({ _id: mongoose.Types.ObjectId(userId) });
+	res.json(user);
 });
 
 const modifyUser = AsyncErrorHandler(async (req, res) => {
-  const { userId, role } = req.body;
+	const { userId, role } = req.body;
 
-  if (!userId || !role) {
-    res.status(400);
-  }
+	if (!userId || !role) {
+		res.status(400);
+	}
 
-  if (role !== "librarian" || role !== "user") {
-    res.status(400);
-  }
+	if (role !== "librarian" || role !== "user") {
+		res.status(400);
+	}
 
-  const updateUser = await Users.findOneAndUpdate(
-    { _id: mongoose.Types.ObjectId(userId) },
-    { role: findRole(role) }
-  );
+	const updateUser = await Users.findOneAndUpdate(
+		{ _id: mongoose.Types.ObjectId(userId) },
+		{ role: findRole(role) }
+	);
 
-  console.log(updateUser);
+	console.log(updateUser);
 });
 
 const createLibrary = AsyncErrorHandler(async (req, res) => {
-  const { name, location, contactNo, contactEmail, maxBooks, issuePeriod, librarianEmail, fineInterest } = req.body;
+	const {
+		name,
+		location,
+		contactNo,
+		contactEmail,
+		maxBooks,
+		issuePeriod,
+		librarianEmail,
+		fineInterest,
+	} = req.body;
 
-  if (!name || !location || !contactNo || !contactEmail || !maxBooks || !issuePeriod || !librarianEmail || !fineInterest) {
-    res.status(400).json({
-      message: "All fields are required",
-    });
-  }
-  let librarian = await Users.find({ email: librarianEmail });
-  if (!librarian) {
-    librarian = new Users({
-      email: librarianEmail,
-      password: generateStrongPassword(),
-      role: process.env.LIBRARIAN_KEY,
-      adminId: req.user,
-    })
+	if (
+		!name ||
+		!location ||
+		!contactNo ||
+		!contactEmail ||
+		!maxBooks ||
+		!issuePeriod ||
+		!librarianEmail ||
+		!fineInterest
+	) {
+		res.status(400).json({
+			message: "All fields are required",
+		});
+	}
+	let librarian = await Users.find({ email: librarianEmail });
+	if (!librarian) {
+		librarian = new Users({
+			email: librarianEmail,
+			password: generateStrongPassword(),
+			role: process.env.LIBRARIAN_KEY,
+			adminId: req.user,
+		});
 
-    await librarian.save();
-  }
+		await librarian.save();
+	}
 
-  const newLibrary = new Library({
-    name,
-    location,
-    contactNo,
-    contactEmail,
-    totalBooks: 0,
-    adminId: req.user,
-    issuePeriod,
-    maxBooks,
-    fineInterest,
-    librarians: librarian._id,
-  });
-  await newLibrary.save();
-  res.json(newLibrary);
+	const newLibrary = new Library({
+		name,
+		location,
+		contactNo,
+		contactEmail,
+		totalBooks: 0,
+		adminId: req.user,
+		issuePeriod,
+		maxBooks,
+		fineInterest,
+		librarians: librarian._id,
+	});
+	await newLibrary.save();
+	res.json(newLibrary);
 });
 
 const updateLibrary = AsyncErrorHandler(async (req, res) => {
-  const { libraryId } = req.params;
+	const { libraryId } = req.params;
 
-  if (!libraryId) {
-    res.status(400);
-  }
+	if (!libraryId) {
+		res.status(400);
+	}
 
-  const library = await Library.findById({ _id: mongoose.Types.ObjectId(libraryId) });
-  if (!library) {
-    res.status(400).json({ message: "Library not found" });
-  }
+	const library = await Library.findById({
+		_id: mongoose.Types.ObjectId(libraryId),
+	});
+	if (!library) {
+		res.status(400).json({ message: "Library not found" });
+	}
 
-  let { name, location, contactNo, contactEmail, maxBooks, issuePeriod, librarianEmail, fineInterest } = req.body;
+	let {
+		name,
+		location,
+		contactNo,
+		contactEmail,
+		maxBooks,
+		issuePeriod,
+		librarianEmail,
+		fineInterest,
+	} = req.body;
 
-  let librarian = await Users.findOne({ email: librarianEmail });
-  if (!librarian) {
-    librarian = new Users({
-      email: librarianEmail,
-      password: generateStrongPassword(),
-      role: process.env.LIBRARIAN_KEY,
-      adminId: req.user,
-    })
+	let librarian = await Users.findOne({ email: librarianEmail });
+	if (!librarian) {
+		librarian = new Users({
+			email: librarianEmail,
+			password: generateStrongPassword(),
+			role: process.env.LIBRARIAN_KEY,
+			adminId: req.user,
+		});
 
-    await librarian.save();
-  }
+		await librarian.save();
+	}
 
-  name = name || library.name;
-  location = location || library.location;
-  contactNo = contactNo || library.contactNo;
-  contactEmail = contactEmail || library.contactEmail;
-  maxBooks = maxBooks || library.maxBooks;
-  issuePeriod = issuePeriod || library.issuePeriod;
-  fineInterest = fineInterest || library.fineInterest;
-  
-  const updateLibrary = await Library.findByIdAndUpdate(
-    { _id: mongoose.Types.ObjectId(libraryId) },
-    {
-      name,
-      location,
-      contactNo,
-      contactEmail,
-      totalBooks: 0,
-      adminId: req.user,
-      issuePeriod,
-      maxBooks,
-      fineInterest,
-      librarians: librarian._id,
-    }
-  );
+	name = name || library.name;
+	location = location || library.location;
+	contactNo = contactNo || library.contactNo;
+	contactEmail = contactEmail || library.contactEmail;
+	maxBooks = maxBooks || library.maxBooks;
+	issuePeriod = issuePeriod || library.issuePeriod;
+	fineInterest = fineInterest || library.fineInterest;
 
-  if (!updateLibrary) {
-    res.status(400).json({ message: "Library not Updated" });
-  }
-  res.json(updateLibrary);
+	const updateLibrary = await Library.findByIdAndUpdate(
+		{ _id: mongoose.Types.ObjectId(libraryId) },
+		{
+			name,
+			location,
+			contactNo,
+			contactEmail,
+			totalBooks: 0,
+			adminId: req.user,
+			issuePeriod,
+			maxBooks,
+			fineInterest,
+			librarians: librarian._id,
+		}
+	);
+
+	if (!updateLibrary) {
+		res.status(400).json({ message: "Library not Updated" });
+	}
+	res.json(updateLibrary);
 });
 
-
 const createLibrarian = AsyncErrorHandler(async (req, res) => {
-  const { name, email } = req.body;
+	const { name, email } = req.body;
 
-  if (!name || !email) {
-    res.status(400).json({
-      message: "All fields are required",
-    });
-  }
-  const existingUser = await Users.findOne({ email });
-  if (existingUser) {
-    res.status(400).json({ message: "Librarian already exists" });
-  }
-  const password = generateStrongPassword();
+	if (!name || !email) {
+		res.status(400).json({
+			message: "All fields are required",
+		});
+	}
+	const existingUser = await Users.findOne({ email });
+	if (existingUser) {
+		res.status(400).json({ message: "Librarian already exists" });
+	}
+	const password = generateStrongPassword();
 
-  const newLibrarian = new Users({
-    name,
-    email,
-    password,
-    role: process.env.LIBRARIAN_KEY,
-    adminId: req.user,
-  });
-  await newLibrarian.save();
-  sendWelcomeEmail(email, password, name);
-  res.json({ newLibrarian, password });
+	const newLibrarian = new Users({
+		name,
+		email,
+		password,
+		role: process.env.LIBRARIAN_KEY,
+		adminId: req.user,
+	});
+	await newLibrarian.save();
+	sendWelcomeEmail(email, password, name);
+	res.json({ newLibrarian, password });
 });
 
 const createMultipleLibrarians = AsyncErrorHandler(async (req, res) => {
-  const { librarians } = req.body;
-  if (!librarians || !Array.isArray(librarians)) {
-    return res.status(400).json({ message: "Invalid input data" });
-  }
-  const createdLibrarians = [];
-  const emailContent = [];
-  for (const librarian of librarians) {
-    const password = generateStrongPassword();
-    emailContent.push({
-      email: librarian.email,
-      password,
-      name: librarian.name,
-    });
-    createdLibrarians.push({
-      name: librarian.name,
-      email: librarian.email,
-      password,
-      role: process.env.LIBRARIAN_KEY,
-      adminId: req.user,
-    });
-  }
-  for (const email of emailContent) {
-    sendWelcomeEmail(email.email, email.password, email.name);
-  }
-  await Users.insertMany(createdLibrarians);
+	const { librarians } = req.body;
+	if (!librarians || !Array.isArray(librarians)) {
+		return res.status(400).json({ message: "Invalid input data" });
+	}
+	const createdLibrarians = [];
+	const emailContent = [];
+	for (const librarian of librarians) {
+		const password = generateStrongPassword();
+		emailContent.push({
+			email: librarian.email,
+			password,
+			name: librarian.name,
+		});
+		createdLibrarians.push({
+			name: librarian.name,
+			email: librarian.email,
+			password,
+			role: process.env.LIBRARIAN_KEY,
+			adminId: req.user,
+		});
+	}
+	for (const email of emailContent) {
+		sendWelcomeEmail(email.email, email.password, email.name);
+	}
+	await Users.insertMany(createdLibrarians);
 
-  res.json({ createdLibrarians });
+	res.json({ createdLibrarians });
 });
 
 const registerAdmin = AsyncErrorHandler(async (req, res) => {
-  const { name, email, password } = req.body;
-  if (!name || !email || !password) {
-    res.status(400).json({
-      message: "All fields are required",
-    });
-  }
+	const { name, email, password } = req.body;
+	if (!name || !email || !password) {
+		res.status(400).json({
+			message: "All fields are required",
+		});
+	}
 
-  if (await Users.findOne({ email })) {
-    res.status(400).json({
-      message: "Admin already exists",
-    });
-  }
+	if (await Users.findOne({ email })) {
+		res.status(400).json({
+			message: "Admin already exists",
+		});
+	}
 
-  const admin = await Users.create({
-    name,
-    email,
-    password,
-    role: process.env.ADMIN_KEY,
-  });
+	const admin = await Users.create({
+		name,
+		email,
+		password,
+		role: process.env.ADMIN_KEY,
+	});
 
-  const token = jwt.sign({ id: admin._id }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
-  });
+	const token = jwt.sign({ id: admin._id }, process.env.JWT_SECRET, {
+		expiresIn: "7d",
+	});
 
-  res.json({ token, message: "Admin created successfully" });
+	res.json({ token, message: "Admin created successfully" });
 });
 
 const loginAdmin = AsyncErrorHandler(async (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    res.status(400).json({ message: "All fields are required" });
-  }
-  const user = await Users.findOne({ email });
-  if (!user) {
-    res.status(404).json({ message: "User not found" });
-  }
-  if (user.role !== process.env.ADMIN_KEY) {
-    res.status(400).json({ message: "Not an admin" });
-  }
+	const { email, password } = req.body;
+	if (!email || !password) {
+		res.status(400).json({ message: "All fields are required" });
+	}
+	const user = await Users.findOne({ email });
+	if (!user) {
+		res.status(404).json({ message: "User not found" });
+	}
+	if (user.role !== process.env.ADMIN_KEY) {
+		res.status(400).json({ message: "Not an admin" });
+	}
 
-  if (!(await user.matchPassword(password))) {
-    res.status(400).json({ message: "Invalid credentials" });
-  }
+	if (!(await user.matchPassword(password))) {
+		res.status(400).json({ message: "Invalid credentials" });
+	}
 
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
-  });
-  console.log(token);
-  res.json({ token, message: "Admin logged in successfully" });
+	const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+		expiresIn: "7d",
+	});
+	console.log(token);
+	res.json({ token, message: "Admin logged in successfully" });
+});
+
+const deleteLibrary = AsyncErrorHandler(async (req, res) => {
+	const { libraryId } = req.params;
+
+	const library = await Library.findById(libraryId);
+
+	if (!library) {
+		return res.status(404).json({
+			message: "Library not found",
+		});
+	}
+
+	await library.remove();
+	res.json({ message: "Library deleted successfully" });
 });
 
 export {
-  modifyUser,
-  getAllUsers,
-  getSpecificUser,
-  createLibrary,
-  deleteLibrary,
-  updateLibrary,
-  createLibrarian,
-  createMultipleLibrarians,
-  registerAdmin,
-  loginAdmin,
+	modifyUser,
+	getAllUsers,
+	getSpecificUser,
+	createLibrary,
+	deleteLibrary,
+	updateLibrary,
+	createLibrarian,
+	createMultipleLibrarians,
+	registerAdmin,
+	loginAdmin,
 };
--- _routes/users/users.routes.js
+++ _routes/users/users.routes.js
@@ -1,30 +1,19 @@
 import express from "express";
 // Ashi's Functions
 import {
-  getBookDetails,
-  getBooks,
-  rateBook,
-  issueBookToUser,
-  checkAvailability,
-  requestRenewal,
-  reportLostFine,
-  getFines,
-  payFine,
+	getBookDetails,
+	getBooks,
+	rateBook,
+	issueBookToUser,
+	checkAvailability,
 } from "../../_controllers/users/AllBooks.js";
 
 // Ashu's Functions
 import {
-  getUser,
-  loginUser,
-  updatePassword,
-  getUserIssues,
 	getUser,
 	loginUser,
 	updatePassword,
 	getUserIssues,
-	payFineForLostBook,
-	requestFinePaymentForOverdueBook,
-	payFineForOverdueBook
 } from "../../_controllers/users/userController.js";
 
 import { protect } from "../../middlewares/authMiddleware.js";
@@ -37,10 +26,6 @@ router.post("/login", loginUser);
 router.post("/updatePassword", protect, updatePassword);
 router.get("/getUserIssues", protect, getUserIssues);
 router.post("/checkAvailability", protect, checkAvailability);
-router.post("/requestRenewal", protect, requestRenewal);
-router.post("/reportLostFine", protect, reportLostFine);
-router.get("/getFines", protect, getFines);
-router.post("/payFine", protect, payFine);
 
 //Ashi's Routes
 //Get details of single book
@@ -55,16 +40,6 @@ router.post("/rateBook/:id", protect, rateBook); //p
 //Issue book to user
 router.post("/issueBook", protect, issueBookToUser); //done
 
-//Fine for lost book
-router.post("/payFineForLostBook",protect, payFineForLostBook);
-
+export default router;
 
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2NTgwN2YyN2NiZGFlMzRhZmNhNWE2NiIsImlhdCI6MTcxNzA0ODUwNiwiZXhwIjoxNzE3NjUzMzA2fQ.sneJPD2KxJWY3QTho2lzPoGFZwNTKmhjh_9ZjBcK2ko
-
-//Req for fine payment
-router.post("/requestFinePayment",protect, requestFinePaymentForOverdueBook);
-
-//Pay fine after getting approved
-router.post("/payFine" , protect , payFineForOverdueBook);
-
-export default router;
